fff2840e62006ec166f634b9b4e88c4bedf3e39b
Trashman1971 <sean.mellett@bluehills.org>
2025-05-01 11:32:05 -0400

    Update piece.py

--- piece.py
+++ piece.py
@@ -1,6 +1,14 @@
 class piece:
-    def __init__(self, screen, color, type):
+    def __init__(self, screen, color,position, is_king):
         self.screen = screen
         self.color = color
-        self.type = type
-    
+        self.position = position
+        self.is_king = is_king
+    def valid_moves(self):
+        if self.color == (92, 64, 51):# bottom white
+            pass
+        elif self.color == (28, 28, 27):#top black
+            pass
+            #return self.position[0]+1, self.position[1]+1
+    def get_pos(self):
+        return self.position
